97215fecbae0118dc3740e3fba1ecac427356160
David Kral <david.k.kral@oracle.com>
2020-11-19 17:02:38 +0100

    minor updates
    Signed-off-by: David Kral <david.k.kral@oracle.com>

--- src/main/java/org/eclipse/yasson/internal/processor/types/AbstractNumberDeserializer.java
+++ src/main/java/org/eclipse/yasson/internal/processor/types/AbstractNumberDeserializer.java
@@ -2,8 +2,11 @@ package org.eclipse.yasson.internal.processor.types;
 
 import java.lang.reflect.Type;
 import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.text.NumberFormat;
 import java.text.ParseException;
+import java.util.Locale;
+import java.util.function.Function;
 
 import jakarta.json.bind.JsonbException;
 import org.eclipse.yasson.internal.model.customization.Customization;
@@ -41,16 +44,29 @@ abstract class AbstractNumberDeserializer<T extends Number> extends TypeDeserial
 
         final JsonbNumberFormatter numberFormat = customization.getDeserializeNumberFormatter();
         //consider synchronizing on format instance or per thread cache.
-        final NumberFormat format = NumberFormat.getInstance(builder.getConfigProperties().getLocale(numberFormat.getLocale()));
+        Locale locale = builder.getConfigProperties().getLocale(numberFormat.getLocale());
+        final NumberFormat format = NumberFormat.getInstance(locale);
         ((DecimalFormat) format).applyPattern(numberFormat.getFormat());
         format.setParseIntegerOnly(integerOnly);
+        Function<String, String> valueChanger = createCompatibilityValueChanger(locale);
         return (value, context) -> {
             try {
-                return parseNumberValue(String.valueOf(format.parse(value)));
+                String updated = valueChanger.apply(value);
+                return parseNumberValue(String.valueOf(format.parse(updated)));
             } catch (ParseException e) {
                 throw new JsonbException(Messages.getMessage(MessageKeys.PARSING_NUMBER, value, numberFormat.getFormat()));
             }
         };
+    }
+
+    private Function<String, String> createCompatibilityValueChanger(Locale locale) {
+        char beforeJdk13GroupSeparator = '\u00A0';
+        char frenchGroupingSeparator = DecimalFormatSymbols.getInstance(Locale.FRENCH).getGroupingSeparator();
+        if (locale.getLanguage().equals(Locale.FRENCH.getLanguage()) && beforeJdk13GroupSeparator != frenchGroupingSeparator) {
+            //JDK-8225245
+            return value -> value.replace(beforeJdk13GroupSeparator, frenchGroupingSeparator);
+        }
+        return value -> value;
     }
 
     abstract T parseNumberValue(String value);
