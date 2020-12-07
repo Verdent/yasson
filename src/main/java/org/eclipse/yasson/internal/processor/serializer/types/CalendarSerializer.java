f2380084d934ce8cee93de14dc52d1af670268ae
David Kral <david.k.kral@oracle.com>
2020-12-07 13:41:40 +0100

    Remaining serializers added
    Signed-off-by: David Kral <david.k.kral@oracle.com>

--- src/main/java/org/eclipse/yasson/internal/processor/serializer/types/CalendarSerializer.java
+++ src/main/java/org/eclipse/yasson/internal/processor/serializer/types/CalendarSerializer.java
@@ -22,18 +22,18 @@ class CalendarSerializer extends AbstractDateSerializer<Calendar> {
     }
 
     @Override
-    protected TemporalAccessor toTemporalAccessor(Calendar value) {
-        return ZonedDateTime.ofInstant(Instant.ofEpochMilli(value.getTimeInMillis()),
-                                       value.getTimeZone().toZoneId());
-    }
-
-    @Override
-    protected DateTimeFormatter defaultFormatter(Calendar value, Locale locale) {
+    protected String formatDefault(Calendar value, Locale locale) {
         DateTimeFormatter formatter = value.isSet(Calendar.HOUR) || value.isSet(Calendar.HOUR_OF_DAY)
                 ? DateTimeFormatter.ISO_DATE_TIME
                 : DateTimeFormatter.ISO_DATE;
         return formatter.withZone(value.getTimeZone().toZoneId())
-                .withLocale(locale);
+                .withLocale(locale).format(toTemporalAccessor(value));
+    }
+
+    @Override
+    protected TemporalAccessor toTemporalAccessor(Calendar object) {
+        return ZonedDateTime.ofInstant(Instant.ofEpochMilli(object.getTimeInMillis()),
+                                       object.getTimeZone().toZoneId());
     }
 
 }
