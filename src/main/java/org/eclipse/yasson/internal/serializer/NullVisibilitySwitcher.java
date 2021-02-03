bd488463ef8a4762e5a922375c9895ceb437f170
David Kral <david.k.kral@oracle.com>
2021-02-03 11:16:13 +0100

    javadoc update
    Signed-off-by: David Kral <david.k.kral@oracle.com>

--- src/main/java/org/eclipse/yasson/internal/serializer/NullVisibilitySwitcher.java
+++ src/main/java/org/eclipse/yasson/internal/serializer/NullVisibilitySwitcher.java
@@ -4,14 +4,18 @@ import jakarta.json.stream.JsonGenerator;
 import org.eclipse.yasson.internal.SerializationContextImpl;
 
 /**
- * TODO javadoc
+ * Switching mechanism for default null value visibility in the JSON.
+ *
+ * Some of the constructs such as arrays, collections etc. require to have nulls serialized into the JSON by default.
+ * This class switches from the default parent null visibility to the current construct visibility. As soon as the current
+ * construct is serialized, visibility is switched back to the parent ones.
  */
 class NullVisibilitySwitcher implements ModelSerializer {
 
     private final boolean nullsEnabled;
     private final ModelSerializer delegate;
 
-    public NullVisibilitySwitcher(boolean nullsEnabled, ModelSerializer delegate) {
+    NullVisibilitySwitcher(boolean nullsEnabled, ModelSerializer delegate) {
         this.nullsEnabled = nullsEnabled;
         this.delegate = delegate;
     }
