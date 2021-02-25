4fb276500cf81b676e5113fd20708cd048b348e5
David Kral <david.k.kral@oracle.com>
2021-02-25 10:36:32 +0100

    records support added customization builders are now included in proper classes
    Signed-off-by: David Kral <david.k.kral@oracle.com>

--- src/main/java/org/eclipse/yasson/internal/model/customization/PropertyCustomization.java
+++ src/main/java/org/eclipse/yasson/internal/model/customization/PropertyCustomization.java
@@ -33,8 +33,8 @@ public class PropertyCustomization extends CustomizationBase {
     private final AdapterBinding serializeAdapter;
     private final AdapterBinding deserializeAdapter;
 
-    private boolean readTransient;
-    private boolean writeTransient;
+    private final boolean readTransient;
+    private final boolean writeTransient;
 
     private final Class<?> implementationClass;
 
@@ -43,19 +43,23 @@ public class PropertyCustomization extends CustomizationBase {
      *
      * @param builder not null
      */
-    public PropertyCustomization(PropertyCustomizationBuilder builder) {
+    private PropertyCustomization(Builder builder) {
         super(builder);
-        this.serializeAdapter = builder.getSerializeAdapter();
-        this.deserializeAdapter = builder.getDeserializeAdapter();
-        this.jsonReadName = builder.getJsonReadName();
-        this.jsonWriteName = builder.getJsonWriteName();
-        this.serializeNumberFormatter = builder.getSerializeNumberFormatter();
-        this.deserializeNumberFormatter = builder.getDeserializeNumberFormatter();
-        this.serializeDateFormatter = builder.getSerializeDateFormatter();
-        this.deserializeDateFormatter = builder.getDeserializeDateFormatter();
-        this.readTransient = builder.isReadTransient();
-        this.writeTransient = builder.isWriteTransient();
-        this.implementationClass = builder.getImplementationClass();
+        this.serializeAdapter = builder.serializeAdapter;
+        this.deserializeAdapter = builder.deserializeAdapter;
+        this.jsonReadName = builder.jsonReadName;
+        this.jsonWriteName = builder.jsonWriteName;
+        this.serializeNumberFormatter = builder.serializeNumberFormatter;
+        this.deserializeNumberFormatter = builder.deserializeNumberFormatter;
+        this.serializeDateFormatter = builder.serializeDateFormatter;
+        this.deserializeDateFormatter = builder.deserializeDateFormatter;
+        this.readTransient = builder.readTransient;
+        this.writeTransient = builder.writeTransient;
+        this.implementationClass = builder.implementationClass;
+    }
+
+    public static Builder builder() {
+        return new Builder();
     }
 
     /**
@@ -135,6 +139,154 @@ public class PropertyCustomization extends CustomizationBase {
     @Override
     public AdapterBinding getSerializeAdapterBinding() {
         return serializeAdapter;
+    }
+
+    public static final class Builder extends CustomizationBase.Builder<Builder, PropertyCustomization> {
+
+        private String jsonReadName;
+        private String jsonWriteName;
+        private JsonbNumberFormatter serializeNumberFormatter;
+        private JsonbNumberFormatter deserializeNumberFormatter;
+        private JsonbDateFormatter serializeDateFormatter;
+        private JsonbDateFormatter deserializeDateFormatter;
+        private AdapterBinding serializeAdapter;
+        private AdapterBinding deserializeAdapter;
+        private boolean readTransient;
+        private boolean writeTransient;
+        private Class<?> implementationClass;
+
+        private Builder() {
+        }
+
+        @Override
+        public Builder copyFrom(PropertyCustomization customization) {
+            jsonReadName = customization.jsonReadName;
+            jsonWriteName = customization.jsonWriteName;
+            serializeNumberFormatter = customization.serializeNumberFormatter;
+            deserializeNumberFormatter = customization.deserializeNumberFormatter;
+            serializeDateFormatter = customization.serializeDateFormatter;
+            deserializeDateFormatter = customization.deserializeDateFormatter;
+            serializeAdapter = customization.serializeAdapter;
+            deserializeAdapter = customization.deserializeAdapter;
+            readTransient = customization.readTransient;
+            writeTransient = customization.writeTransient;
+            implementationClass = customization.implementationClass;
+            return super.copyFrom(customization);
+        }
+
+        /**
+         * Set a JSON property name used to read a property value from on deserialization.
+         *
+         * @param jsonReadName JSON property name
+         */
+        public Builder jsonReadName(String jsonReadName) {
+            this.jsonReadName = jsonReadName;
+            return this;
+        }
+
+        /**
+         * Set a property name which is written to JSON document on serialization.
+         *
+         * @param jsonWriteName Property name.
+         */
+        public Builder jsonWriteName(String jsonWriteName) {
+            this.jsonWriteName = jsonWriteName;
+            return this;
+        }
+
+        /**
+         * Set number formatter for formatting numbers during serialization process.
+         *
+         * @param serializeNumberFormatter Number formatter for formatting numbers during serialization process.
+         */
+        public Builder serializeNumberFormatter(JsonbNumberFormatter serializeNumberFormatter) {
+            this.serializeNumberFormatter = serializeNumberFormatter;
+            return this;
+        }
+
+        /**
+         * Set number formatter for formatting numbers during deserialization process.
+         *
+         * @param deserializeNumberFormatter Number formatter for formatting numbers during deserialization process.
+         */
+        public Builder deserializeNumberFormatter(JsonbNumberFormatter deserializeNumberFormatter) {
+            this.deserializeNumberFormatter = deserializeNumberFormatter;
+            return this;
+        }
+
+        /**
+         * Set date formatter for formatting dates during serialization process.
+         *
+         * @param serializeDateFormatter Date formatter for formatting dates during serialization process.
+         */
+        public Builder serializeDateFormatter(JsonbDateFormatter serializeDateFormatter) {
+            this.serializeDateFormatter = serializeDateFormatter;
+            return this;
+        }
+
+        /**
+         * Set date formatter for formatting dates during deserialization process.
+         *
+         * @param deserializeDateFormatter Date formatter for formatting dates during deserialization process.
+         */
+        public Builder deserializeDateFormatter(JsonbDateFormatter deserializeDateFormatter) {
+            this.deserializeDateFormatter = deserializeDateFormatter;
+            return this;
+        }
+
+        public Builder serializeAdapter(AdapterBinding serializeAdapter) {
+            this.serializeAdapter = serializeAdapter;
+            return this;
+        }
+
+        public Builder deserializeAdapter(AdapterBinding deserializeAdapter) {
+            this.deserializeAdapter = deserializeAdapter;
+            return this;
+        }
+
+        /**
+         * Sets a presence of <i>read transient</i> customization.
+         *
+         * @param readTransient Presence of <i>read transient</i> customization.
+         */
+        public Builder readTransient(boolean readTransient) {
+            this.readTransient = readTransient;
+            return this;
+        }
+
+        public boolean readTransient() {
+            return readTransient;
+        }
+
+        /**
+         * Sets a presence of <i>write transient</i> customization.
+         *
+         * @param writeTransient Presence of <i>write transient</i> customization.
+         */
+        public Builder writeTransient(boolean writeTransient) {
+            this.writeTransient = writeTransient;
+            return this;
+        }
+
+        public boolean writeTransient() {
+            return writeTransient;
+        }
+
+        /**
+         * Implementation class if property is interface type.
+         *
+         * @param implementationClass implementing property interface
+         */
+        public Builder implementationClass(Class<?> implementationClass) {
+            this.implementationClass = implementationClass;
+            return this;
+        }
+
+        @Override
+        public PropertyCustomization build() {
+            return new PropertyCustomization(this);
+        }
+
     }
 
 }
